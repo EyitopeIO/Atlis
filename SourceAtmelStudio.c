b184beb20d91082edf21074260732b4a15d8b910
Eyitope Adelowo <topeboy2009@gmail.com>
2017-09-16 13:57:54 +0100

    Update SourceAtmelStudio.c
    Changed the entire structure of the code. Would use timers to make the calibrating light blink.

--- SourceAtmelStudio.c
+++ SourceAtmelStudio.c
@@ -12,8 +12,9 @@
 
 #include <avr/io.h>
 #include <util/delay_basic.h>
-#define F_CPU 16000000// 8MHz for testing, 16Mhz actual
+#include <avr/eeprom.h>
 
+#define F_CPU 16000000// 8MHz for testing, 16Mhz actual 
 #define calibrating_off PORTB&=~(1<<PINB5) 
 #define calibrating_on PORTB|=(1<<PINB5)
 #define switch_open PORTB&=~(1<<PINB0)
@@ -24,27 +25,41 @@
 #define savedmin_off PORTB&=~(1<<PINB1)
 #define saving_on PORTB|=(1<<PINB7)
 #define saving_off PORTB&=~(1<<PINB7)
+#define MAX 2
+#define NOW 1
+#define MIN 0
 
 
-void blink (char tag, int on_time, int off_time);
-void toggle_switch(void);
-void save_new_max(void);
-void save_new_min(void);
-void calibrate_min(void); 
-void calibrate_max(void);
-void EEPROM_write(unsigned int uiAddress, unsigned char ucData);
-unsigned char EEPROM_read(unsigned int uiAddress);
+/************************************************/
+
+uint16_t begin_statistical_analysis(int number_of_iterations, int number_of_readings);
+void begin_saving_process(int no_of_iterations, int no_of_readings);
+
+
 void InitializeADC(void);
 uint16_t ADCRead(uint8_t ch);
 
-uint8_t ADC_channel = 0;
-const unsigned int max_address = 4; //Value for max address is saved here.
-const unsigned int min_address = 16;
-volatile uint8_t max; 
-volatile uint8_t min;
-volatile unsigned char max_button_value;
+/**********************************************/
+
+//unsigned char EEMEM FIRST_RUN = 0x00; To know if it's the first time this program would ever run.
+
+const uint8_t ADC_channel = 0;
+
+volatile unsigned char max_button_value; //State of the buttons: high or low.
 volatile unsigned char min_button_value;
+
 volatile uint16_t FromADC0; //analog value read from LDR is a 16 bit quantity
+
+/* Here are arrays to keep values to save to EEPROM
+ * I use 16-bit quantities because numbers could range from 0-1023
+*/ 
+uint16_t saved_max[2];  
+uint16_t saved_min[2];
+
+int eeprom_max_address = 0;
+int eeprom_min_address = sizeof(saved_min);
+
+/*************************************************************************/
 
 int main(void)
 {	
@@ -54,7 +69,7 @@ int main(void)
 	DDRB = 0b00100111; //0X27 
 	DDRC = 0b11111110; //0X00
 	DDRD = 0b10011111; //0X9F
-	
+
 	//portB0 = status of switch, pin14
 	//portB1 = min_is_saved indicator, pin15
 	//portB2 = max_is_saved indicator, pin16
@@ -67,145 +82,87 @@ int main(void)
    //Code banking under consideration
 	
 	InitializeADC();
-	max = EEPROM_read(max_address);
-	min = EEPROM_read(min_address);
+	saved_max = eeprom_read_block(); 
+	saved_min = eeprom_read_block();
+	
+	uint16_t statical_results; //Results from statistical analysis
+	
+	int number_of_iterations = 10; //This might increase or decrease (by pointers) based on whether readings are reliable or not.
+	int number_of_readings = 10; //This might also change as above.
+	
+	int *p_n_o_iter = &number_of_iterations;
+	int *p_n_o_read = &number_of_readings;
+
+	unsigned int mid_of_max_and_min = (max + min) / 2; 
+	unsigned char is_day = TRUE; //Works as a boolean to let me know if it's day. We should assume it's day
 	
 	while (1) 
     {
+		
+		// To even out things, whatever number of iterations or readings must be applied to all cases before being changed.
+		
 		max_button_value = PORTD6;
-		max_button_value = PORTD5; 
-		FromADC0 = ADCRead(ADC_channel);
-		toggle_switch();
-		save_new_max(); //This also checks whether the button is pressed or not
-		save_new_min();
-		blink('c', 200, 500);	
-	}
+		min_button_value = PORTD5; 
+		/*Use an interrupt to deal with the button press.
+		*The interrupt should trigger a variable that would be used to save or not save.
+		*/
+		FromADC0.now = begin_statistical_analysis(number_of_iterations, number_of_readings)
+
+		if ( FromADC0 <= saved_min.now ) {
+			statical_results = begin_statistical_analysis(int number_of_iterations, int number_of_readings); //place switch_close in this function
+		}
+		else if (FromADC0 >= saved_max.now ) {
+			statical_results = begin_statistical_analysis(int number_of_iterations, int number_of_readings) //Place switch_open in this
+		}
+	}		
 }
 
-void blink( char PORT, int _on_time, int _off_time) //Use the pin number
-{
-	/*
-	//portB1 = min_is_saved indicator, pin15
-	//portB2 = max_is_saved indicator, pin16
-	//portB5 = calibrating indicator, pin19
-	*/
-	unsigned int off_time = _off_time;
-	unsigned int on_time = _on_time;
-	switch (PORT) {
-		case 'n': //min_is_saved_indicator_light
-			savedmin_on;
-			_delay_loop_2(on_time);
-			savedmin_off;
-			_delay_loop_2(off_time);
-			savedmin_on;
-			_delay_loop_2(on_time);
-			savedmin_off;
-			_delay_loop_2(off_time);
-		case 'm': //max_is_saved_indicator_light
-			savedmax_on;
-			_delay_loop_2(on_time);
-			savedmax_off;
-			_delay_loop_2(off_time);
-			savedmax_on;
-			_delay_loop_2(on_time);
-			savedmax_off;
-			_delay_loop_2(off_time);
-		case 'c': //Callibrating_light
-			calibrating_on;
-			_delay_loop_2(on_time);
-			calibrating_off;
-			_delay_loop_2(off_time);
-			calibrating_on;
-			_delay_loop_2(on_time);
-			calibrating_off;
-			_delay_loop_2(off_time);
-	}
-}
-
+void begin_saving_process (int no_of_iterations, int array_size) {
 	
-void toggle_switch(void)
-{
-	if ( ((FromADC0 - min > -10) && (FromADC0 - min < 10)) && (FromADC0 < min) )
-	//If (difference between FromSesnor and MIN less than 10) and (brightness less than minimum)
-	{
-		switch_close;
+	/*
+	* First parameter is a pointer to the structure. This would either be *p_saved_min or *p_saved_max.
+	* Second parameter is pointer to the address you'd like to write to in the EEPROM.
+	*/
+	
+	int start_max = 0;
+	int start_min = 1023;
+	char tracker;
+	uint16_t temp;
+	
+	saving_on;
+	
+	for (tracker=0; tracker <= no_of_iterations; tracker++ ) {
+		temp = begin_statistical_analysis(no_of_iterations, array_size);
 	}
-	if ( ((FromADC0 - max > -10) && (FromADC0 - max < 10)) && (FromADC0 > max) )
-	//If (difference between FromADC0 and MAX less than 10) and (brightness greater than maximum)
-	{
-		switch_open;
-	}
-
+	
+	saving_off;	
 }
 
-void calibrate_min(void) 
-{
-	calibrating_on; //Turn on the calibrating indicator light
-	uint8_t count;
-	uint8_t temp_min1;
-	uint8_t temp_min2;
-	uint8_t temp_min3;
-	uint8_t temp_min;
-	for (count=0; count!=5000 ; count++) {
-		temp_min1 = ADCRead(ADC_channel);
-		temp_min2 = ADCRead(ADC_channel);
-		temp_min3 = ADCRead(ADC_channel);
-		temp_min = (temp_min1 + temp_min2 + temp_min3) / 3;
-		if (!((temp_min - min > - 10) && (temp_min - min < 10))) //cater for both negative and positive differences
-		{ 	
-			min = temp_min;
+
+uint16_t begin_statistical_analysis(int number_of_readings, int array_size) {
+	
+	/*What this does is get the average of readings for a number of trials*/
+	
+	int total, array_index, tracker;
+	uint16_t Readings[array_size];
+	uint16_t average;
+	
+	for(array_index=0; array_index <= array_size; array_index++) {
+		Readings[array_index] = 0;
+	}
+	array_index = 0;
+	for(tracker=0; tracker < number_of_readings; tracker++) {
+		total = total - Readings[array_index];
+		Readings[array_index] = ADCRead(ADC_channel);
+		total = total + Readings[array_index];
+		if array_index >= array_size {
+			array_index = 0;
 		}
-	}
-	calibrating_off;
-}
-void calibrate_max(void)
-{
-	calibrating_on; //Turn on the calibrating indicator light
-	uint8_t count;
-	uint8_t temp_max1;
-	uint8_t temp_max2;
-	uint8_t temp_max3;
-	uint8_t temp_max;
-	for (count=0; count!=5000 ; count++) {
-		temp_max1 = ADCRead(ADC_channel);
-		temp_max2 = ADCRead(ADC_channel);
-		temp_max3 = ADCRead(ADC_channel);
-		temp_max = (temp_max1 + temp_max2 + temp_max3) / 3;
-		if (!((temp_max - max > - 10) && (temp_max - max < 10))) 
-		{
-			max = temp_max;
-		}
-	}
-	calibrating_off;
+	average = total / array_size;
+	return average;
 }
 
-/* I copied the EEPROM read and write codes below from the data sheet*/
-void EEPROM_write(unsigned int uiAddress, unsigned char ucData)
-{
-	/* Wait for completion of previous write */
-	while(EECR & (1<<EEPE))
-	;
-	/* Set up address and Data Registers */
-	EEAR = uiAddress;
-	EEDR = ucData;
-	/* Write logical one to EEMPE */
-	EECR |= (1<<EEMPE);
-	/* Start eeprom write by setting EEPE */
-	EECR |= (1<<EEPE);
-}
 
-unsigned char EEPROM_read(unsigned int uiAddress)
-{
-	/* Wait for completion of previous write */
-	while(EECR & (1<<EEPE))
-	;
-	/* Set up address register */
-	EEAR = uiAddress;
-	/* Start eeprom read by writing EERE */
-	EECR |= (1<<EERE);
-	/* Return data from Data Register */
-	return EEDR;
-}
 
 void InitializeADC()
 {
@@ -216,11 +173,11 @@ void InitializeADC()
 uint16_t ADCRead (uint8_t ch) //Reading from Channel zero
 {
 	//Select ADC Channel ch must be 0-7
-	ch=ch&0b00000111;
-	ADMUX|=ch;
+	ch = ch & 0b00000111;
+	ADMUX |= ch;
 
 	//Start Single conversion
-	ADCSRA|=(1<<ADSC);
+	ADCSRA |= (1<<ADSC);
 
 	//Wait for conversion to complete
 	while(!(ADCSRA & (1<<ADIF)));
@@ -234,39 +191,4 @@ uint16_t ADCRead (uint8_t ch) //Reading from Channel zero
 
 	return(ADC);
 } 
-
-
-void save_new_max(void) //max_is_saved_indicator_light = B2
-{
-	if (max_button_value == 0)
-	{
-		savedmax_off;
-		_delay_loop_2(500); //Just so that users can see what's happening
-		saving_on;
-		calibrate_max();
-		EEPROM_write(max_address, max);
-		max = EEPROM_read(max_address); //Reset the global value
-		saving_off;
-		blink('m', 200, 500);
-		savedmax_on;
-		
-	}
-}
-
-void save_new_min(void) //min_is_saved_indicator_light = B1
-{
-	if (min_button_value == 0)
-	{
-		savedmin_off;
-		_delay_loop_2(500); //Just so that users can see what's happening
-		saving_on;
-		calibrate_min();
-		EEPROM_write(min_address, min);
-		min = EEPROM_read(min_address); //Reset the global value
-		saving_off;
-		blink('n', 200,500);
-		savedmin_on;
-		
-	}
-}
 
